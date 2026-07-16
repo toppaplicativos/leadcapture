e4fe26863b45f09d9279b1f3bd566f1615616364
kronosinternational <kronosinternational.dev@gmail.com>
2026-07-16 20:33:32 -0300

    chore: bump SW after stock sync deploy (v224)

--- public/service-worker.js
+++ public/service-worker.js
@@ -27,8 +27,8 @@ if (IS_LOCAL_DEV) {
     );
   });
 } else {
-const SHELL_CACHE_NAME = "lead-system-shell-v223-20260716-stock-ops";
-const RUNTIME_CACHE_NAME = "lead-system-runtime-v214-20260716-stock-ops";
+const SHELL_CACHE_NAME = "lead-system-shell-v224-20260716-stock-ops";
+const RUNTIME_CACHE_NAME = "lead-system-runtime-v215-20260716-stock-ops";
 
 function getBasePath() {
   try {

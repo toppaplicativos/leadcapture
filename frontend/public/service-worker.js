24e51206d7e3151d893e369154a3ea338a454c85
kronosinternational <kronosinternational.dev@gmail.com>
2026-07-16 17:29:07 -0300

    chore: bump SW after clickable buttons fix (v219)

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -27,8 +27,8 @@ if (IS_LOCAL_DEV) {
     );
   });
 } else {
-const SHELL_CACHE_NAME = "lead-system-shell-v218-20260716-mob-offer-sound";
-const RUNTIME_CACHE_NAME = "lead-system-runtime-v209-20260716-mob-offer-sound";
+const SHELL_CACHE_NAME = "lead-system-shell-v219-20260716-mob-offer-sound";
+const RUNTIME_CACHE_NAME = "lead-system-runtime-v210-20260716-mob-offer-sound";
 
 function getBasePath() {
   try {

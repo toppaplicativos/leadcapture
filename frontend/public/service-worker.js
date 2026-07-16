776d1636014115d9cdec5476287911766fa30ca6
kronosinternational <kronosinternational.dev@gmail.com>
2026-07-16 00:13:19 -0300

    chore: bump SW after interactive flows deploy

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -27,8 +27,8 @@ if (IS_LOCAL_DEV) {
     );
   });
 } else {
-const SHELL_CACHE_NAME = "lead-system-shell-v208-20260716-mob-offer-sound";
-const RUNTIME_CACHE_NAME = "lead-system-runtime-v199-20260716-mob-offer-sound";
+const SHELL_CACHE_NAME = "lead-system-shell-v209-20260716-mob-offer-sound";
+const RUNTIME_CACHE_NAME = "lead-system-runtime-v200-20260716-mob-offer-sound";
 
 function getBasePath() {
   try {
--- public/service-worker.js
+++ public/service-worker.js
@@ -27,8 +27,8 @@ if (IS_LOCAL_DEV) {
     );
   });
 } else {
-const SHELL_CACHE_NAME = "lead-system-shell-v208-20260716-stock-ops";
-const RUNTIME_CACHE_NAME = "lead-system-runtime-v199-20260716-stock-ops";
+const SHELL_CACHE_NAME = "lead-system-shell-v209-20260716-stock-ops";
+const RUNTIME_CACHE_NAME = "lead-system-runtime-v200-20260716-stock-ops";
 
 function getBasePath() {
   try {

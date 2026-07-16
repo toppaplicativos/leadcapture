b9ce9962db13915fd02b70d6a61f501175d86521
kronosinternational <kronosinternational.dev@gmail.com>
2026-07-15 22:26:40 -0300

    chore: bump SW after flows integration deploy

--- public/service-worker.js
+++ public/service-worker.js
@@ -27,8 +27,8 @@ if (IS_LOCAL_DEV) {
     );
   });
 } else {
-const SHELL_CACHE_NAME = "lead-system-shell-v207-20260716-stock-ops";
-const RUNTIME_CACHE_NAME = "lead-system-runtime-v198-20260716-stock-ops";
+const SHELL_CACHE_NAME = "lead-system-shell-v208-20260716-stock-ops";
+const RUNTIME_CACHE_NAME = "lead-system-runtime-v199-20260716-stock-ops";
 
 function getBasePath() {
   try {
